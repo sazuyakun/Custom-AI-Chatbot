34df697446f0e3f72eff7a78ded53b9a989e475e
Soham Samal <sohamsamal37@gmail.com>
2024-06-30 01:00:47 +0530

    Creating the frontend and API for the chatbot

--- Chat-bot.py
+++ Chat-bot.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, jsonify
-from flask_restful import Api, Resource
+import pickle
+from flask import Flask, request, jsonify, render_template
+# from flask_restful import Api, Resource
 from langchain_community.document_loaders import WebBaseLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import OllamaEmbeddings
@@ -8,7 +9,6 @@ from langchain_community.llms import Ollama
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
-
 
 url = "https://brainlox.com/courses/category/technical"
 loader = WebBaseLoader(url)
@@ -21,7 +21,12 @@ embeddings = OllamaEmbeddings(
     model="llama3"
 )
 
-vector = FAISS.from_documents(docs, embeddings)
+# vector = FAISS.from_documents(docs, embeddings)
+# import pickle
+# with open("FAISS_Store.pkl", "wb") as file:
+#     pickle.dump(vector, file)
+with open('FAISS_Store.pkl', 'rb') as file:
+    vector = pickle.load(file)
 
 llm = Ollama(
     model="llama3"
@@ -44,24 +49,42 @@ retrieval_chain = create_retrieval_chain(
     retriever,
     document_chain
 )
+def prompt_answer(prompt):
+    response = retrieval_chain.invoke({"input": prompt})
+    return response["answer"]
+
+
 
 app = Flask(__name__)
-api = Api(app)
+# api = Api(app)
 
-class Chatbot(Resource):
-    def post(self):
-        # Parse the input data
-        data = request.get_json()
-        question = data.get('question')
+# class Chatbot(Resource):
+#     def post(self):
+#         # Parse the input data
+#         data = request.get_json()
+#         question = data.get('question')
         
-        # Get the response from the retrieval chain
-        response = retrieval_chain.invoke({
-            "input": question
-        })
+#         # Get the response from the retrieval chain
+#         response = retrieval_chain.invoke({
+#             "input": question
+#         })
         
-        return jsonify({'response': response})
+#         return jsonify({'response': response})
     
-api.add_resource(Chatbot, '/chatbot')
+# api.add_resource(Chatbot, '/chatbot')
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.json
+    prompt = data.get('prompt')
+    if not prompt:
+        return jsonify({"error": "No prompt provided"}), 400
+    answer = prompt_answer(prompt)
+    return jsonify({"answer": answer})
 
 if __name__ == '__main__':
     app.run(debug=True)
